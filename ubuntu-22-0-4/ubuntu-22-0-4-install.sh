aabc4afb4d3893df84b188b7c90f2ccc0a7272d8
koh <koh@uehara.to>
2023-08-04 00:13:55 +0900

    :+1: #2 Add new commands and fix.

--- ubuntu-22-0-4/ubuntu-22-0-4-install.sh
+++ ubuntu-22-0-4/ubuntu-22-0-4-install.sh
@@ -1,17 +1,13 @@
 # TODO: FIXME: XXX: HACK: NOTE: INTENT: USAGE:
 
 sudo apt update
+sudo apt install curl
+sudo apt install htop
+sudo apt install vim
 
 # INTENT: Install oh-my-zsh.
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# INTENT: Setup .zshrc and .zprofile
-mv ~/.zshrc ~/.zshrc.bak
-cp ubuntu-zshrc ~/.zshrc
-cp ubuntu-zprofile ~/.zprofile
-cp koh-my-zsh.zsh-theme ~/.oh-my-zsh/themes/koh-my-zsh.zsh-theme
-source ~/.zprofile
 
 # INTENT: Install Homebrew
 sudo apt install build-essential git
@@ -19,6 +15,15 @@ sudo apt install build-essential git
 source ~/.zprofile
 brew doctor
 brew install gcc
+
+sudo chsh -s $(which zsh)
+
+# INTENT: Setup .zshrc and .zprofile
+mv ~/.zshrc ~/.zshrc.bak
+cp ubuntu-zshrc ~/.zshrc
+cp ubuntu-zprofile ~/.zprofile
+cp koh-my-zsh.zsh-theme ~/.oh-my-zsh/themes/koh-my-zsh.zsh-theme
+source ~/.zprofile
 
 # INTENT: Install pyenv and miniconda
 brew install pyenv
@@ -28,3 +33,15 @@ pyenv install miniconda3-3.11-23.5.0-3
 # INTENT: Set commit template.
 cp commit_template ~/.commit_template
 git config --global commit.template ~/.commit_template
+git config --global core.editor vim
+git config --global user.email koh@uehara.to
+git config --global user.name koh
+
+# INTENT: Set ssh to GitHub.
+touch ~/.ssh/config
+echo -e "Host github github.com\n  HostName github.com\n  IdentityFile ~/.ssh/id_rsa\n  User git" >> ~/.ssh/config
+ssh-keygen
+cat ~/.ssh/id_rsa.pub
+
+# INTENT: Install vscode.
+sudo snap install --classic code
